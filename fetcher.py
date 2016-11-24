71d21d473b5c85dea09c2a4f03840eafa7b91dc3
Austin Fuller <auxchar@gmail.com>
2016-11-24 00:32:36 -0600

    Was skipping updating old posts when one threw a 401 because it was deleted. Fixed by skipping exceptions on individual posts, rather than the entire cycle.

--- fetcher.py
+++ fetcher.py
@@ -6,6 +6,21 @@ import time
 r = praw.Reddit("Reddit Watch by /u/auxiliary-character v 1.0.0")
 r_all = r.get_subreddit("all")
 
+def ignore_exception_call(f, *args):
+    try:
+        f(*args)
+    except Exception as e:
+        print(e)
+        print("Error! Skipping.")
+        time.sleep(10)
+
+def fetch_post(postid):
+    p = r.get_submission(submission_id = postid)
+    db.insert_point(p)
+    db.update_post(postid)
+    if (time.time() - p.created_utc) > 600 and p.score < 2:
+        db.cull_post(postid)
+
 def fetch_new_posts():
     print("Fetching new posts.", time.time())
     for post in r_all.get_new():
@@ -16,22 +31,13 @@ def fetch_old_posts():
     print("Fetching old posts.", time.time())
     ids = [post["postid"] for post in db.get_active_posts()]
     for postid in ids:
-        p = r.get_submission(submission_id = postid)
-        db.insert_point(p)
-        db.update_post(postid)
-        if (time.time() - p.created_utc) > 600 and p.score < 2:
-            db.cull_post(postid)
+        ignore_exception_call(fetch_post, postid)
 
 def main():
     db.create_db()
     while True:
-        try:
-            fetch_new_posts()
-            fetch_old_posts()
-        except Exception as e:
-            print(e)
-            print("Error! Skipping.")
-            time.sleep(10)
+        ignore_exception_call(fetch_new_posts)
+        fetch_old_posts()
 
 if __name__ == "__main__":
     main()
